977c30b075b7e5f541c3ca74176e3ed6c89b4f12
Jacqueline1603 <jacqueline16utt@gmail.com>
2026-03-17 10:58:26 -0600

    Cambio Login

--- utt_horarios/settings.py
+++ utt_horarios/settings.py
@@ -116,9 +116,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
 ]
-
 # ===============================
 # USER PERSONALIZADO
 # ===============================
